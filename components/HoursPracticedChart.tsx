c558efa96044e1bf4b19b18725acb64ff556ac84
Shantanu Raj <s@sraj.me>
2021-11-03 19:19:14 +0100

    🌈 Make colors dynamic
    - Use dynamic hook that resets the color on prefrs-color-scheme query
      changes
    - Pass colors as an argument to the configuration builders
    - Previously the css portions of the UI would update on theme preference
      change but JS land would still retain old colors object

--- components/HoursPracticedChart.tsx
+++ components/HoursPracticedChart.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Asana, State, TimeEntry } from '../types';
+import { Asana, TimeEntry } from '../types';
 import { ChartCanvas } from './ChartCanvas';
 import groupBy from 'lodash.groupby';
-import { getColors } from '../data/colors';
+import { Colors, useColors } from '../data/colors';
 import { ChartConfiguration } from 'chart.js/auto';
 
-export function getHoursPracticedPerMonthBarChart(asana: Asana[], timeEntries: TimeEntry[]): ChartConfiguration<'bar' | 'line'> {
+export function getHoursPracticedPerMonthBarChart(
+    asana: Asana[],
+    timeEntries: TimeEntry[],
+    colors: Colors,
+): ChartConfiguration<'bar' | 'line'> {
     const groupFormatting = { month: 'short' } as const;
     const groupedTimeEntries = groupBy(timeEntries, (te) => te.startDate.toLocaleString('en-us', groupFormatting));
     const hoursPracticedPerMonth = Object.fromEntries(
@@ -14,66 +18,64 @@ export function getHoursPracticedPerMonthBarChart(asana: Asana[], timeEntries: T
         ),
     );
 
-    const colors = getColors()
-
     return {
         type: 'bar',
-        data: {
+            data: {
             labels: Object.keys(hoursPracticedPerMonth),
-            datasets: [
-                {
-                    type: 'line',
-                    data: Object.values(hoursPracticedPerMonth),
-                    fill: false,
-                    backgroundColor: colors.foreground,
-                    tension: 0.1,
-                },
-                {
-                    type: 'bar',
-                    data: Object.values(hoursPracticedPerMonth),
-                    backgroundColor: colors.foreground,
-                },
-            ],
+                datasets: [
+                    {
+                        type: 'line',
+                        data: Object.values(hoursPracticedPerMonth),
+                        fill: false,
+                        backgroundColor: colors.foreground,
+                        tension: 0.1,
+                    },
+                    {
+                        type: 'bar',
+                        data: Object.values(hoursPracticedPerMonth),
+                        backgroundColor: colors.foreground,
+                    },
+                ],
         },
         options: {
             responsive: true,
-            plugins: {
+                plugins: {
                 tooltip: {
                     callbacks: {
                         title: (data) => 'Hours practiced in ' + data[0].label,
-                        afterLabel: (context) => {
-                            const month = context.parsed.x;
-                            const asanaLearnedInMonth = asana
-                                .filter((a) => a.dateLearned.getMonth() == month)
-                                .map((a) => a.name);
+                            afterLabel: (context) => {
+                                const month = context.parsed.x;
+                                const asanaLearnedInMonth = asana
+                                    .filter((a) => a.dateLearned.getMonth() == month)
+                                    .map((a) => a.name);
 
-                            return asanaLearnedInMonth.length == 0
-                                ? 'No new asana learned in that month'
-                                : 'Asana learned in that month: ' + asanaLearnedInMonth.join(', ');
-                        },
-                        label: (context) => {
-                            const rawHours = context.parsed.y;
-                            const fullHours = Math.trunc(rawHours);
-                            const remainder = rawHours - fullHours;
-                            const minutes = Math.trunc(60 * remainder);
+                                return asanaLearnedInMonth.length == 0
+                                    ? 'No new asana learned in that month'
+                                    : 'Asana learned in that month: ' + asanaLearnedInMonth.join(', ');
+                            },
+                                label: (context) => {
+                                    const rawHours = context.parsed.y;
+                                    const fullHours = Math.trunc(rawHours);
+                                    const remainder = rawHours - fullHours;
+                                    const minutes = Math.trunc(60 * remainder);
 
-                            return fullHours + 'h' + String(minutes).padStart(2, '0');
-                        },
+                                    return fullHours + 'h' + String(minutes).padStart(2, '0');
+                                },
                     },
                 },
                 legend: { display: false },
             },
             scales: {
                 x: {
-                    grid: { 
+                    grid: {
                         display: false,
-                        color: colors.chartBorder
+                            color: colors.chartBorder
                     },
                     ticks: {
                         color: colors.onBackground
                     }
                 },
-                y : {
+                y: {
                     grid: {
                         color: colors.chartBorder
                     },
@@ -92,10 +94,11 @@ interface HoursPracticedChartProps {
 }
 
 export const HoursPracticedChart = ({ timeEntries, asana }: HoursPracticedChartProps) => {
+    const colors = useColors();
     return (
         <React.Fragment>
             <h1>Hours Practiced</h1>
-            <ChartCanvas {...getHoursPracticedPerMonthBarChart(asana, timeEntries)} />
+            <ChartCanvas {...getHoursPracticedPerMonthBarChart(asana, timeEntries, colors)} />
         </React.Fragment>
     );
 };
