0bc8ce86a0f590db6f2e94bd3422ebcd2e57133d
William Barbosa <heytherewill@gmail.com>
2021-11-25 05:56:47 +0100

    📣 Description

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,11 @@ const Home: NextPage = () => {
                 {!state && <h1>Loading...</h1>}
                 {state && (
                     <React.Fragment>
+                        <h1> { "Will's Ashtanga Stats" } </h1>
+                        <b> { "As a practitioner of the Mysore style of Ashtanga Yoga," } </b>
+                        <b> { "I (try to) practice the asana sequences 6 times a week." } </b>
+                        <b> { "As a time tracking nerd, I have a ton of data about all my practice sessions." } </b>
+                        <b> { "This page contains some charts extracted from that data." } </b>
                         <HoursPracticedBarChart asana={state.asana} timeEntries={state.timeEntries} />
                         <TypeOfPracticeDoughnutChart timeEntries={state.timeEntries} />
                         <DaysPracticedMatrixChart timeEntries={state.timeEntries} moondays={state.moondays} />
